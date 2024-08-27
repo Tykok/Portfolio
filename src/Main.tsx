fa6d1ec575343c25e9afa34a4eb18d983191c449
Tykok <treportelie12@gmail.com>
2024-08-28 01:19:38 +0200

    [Feat] Use flex for TaskBar

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,19 +1,18 @@
-import TaskBar from './components/TaskBar/TaskBar';
-import LangProvider from './context/LangContext';
+import TaskBar from './components/TaskBar/TaskBar'
+import LangProvider from './context/LangContext'
 
 function Main() {
-
   return (
     <>
-    <LangProvider>
-      <ul>
-        <li>Ajouter un composant icônes</li>
-        <li>Ajouter les icônes à la chaîne avec leurs images</li>
-      </ul>
-      <TaskBar/>
-    </LangProvider>
+      <LangProvider>
+        <ul>
+          <li>Ajouter un composant icônes</li>
+          <li>Ajouter les icônes à la chaîne avec leurs images</li>
+        </ul>
+        <TaskBar />
+      </LangProvider>
     </>
-  );
+  )
 }
 
-export default Main;
+export default Main
